e56bcb17c585e54e25d156e6805aa17df6e106b5
nondanee <iminezn5656@outlook.com>
2026-01-10 20:04:42 +0800

    feat: index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const { spawnSync } = require('child_process');
+// const { promisify } = require('util');
+
+const install = async (version) => {
+
+};
+
+const use = async (version) => {
+
+};
+
+const main = () => {
+
+};
+
+main();
