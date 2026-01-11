df1b0a0163efa526745b5118e3d34acfdb08478e
nondanee <iminezn5656@outlook.com>
2026-01-11 20:23:08 +0800

    feat: 在 node 里做吧

--- src/autoload.js
+++ src/autoload.js
@@ -1,29 +1,6 @@
 module.exports = `
-find_up() {
-    local path=$(pwd)
-    while [[ "$path" != "/" && "$path" != "$HOME" ]]; do
-        for file in "$@"; do
-            if [[ -f "$path/$file" ]]; then
-                echo "$path/$file"
-                return
-            fi
-        done
-        path=$(dirname "$path")
-    done
-}
-
-# 自动切换版本的钩子函数
 _nvm2_autoload_hook() {
-    # 查找 .node-version 或 .nvmrc 文件
-    local node_version_file="$(find_up .node-version .nvmrc)"
-    
-    if [ -f "$node_version_file" ]; then
-        local node_version="$(cat "$node_version_file")"
-        # 如果当前版本与文件指定版本不同，则切换
-        if [ "$(nvm2 current)" != "$node_version" ]; then
-            nvm2 use "$node_version" --silent-if-unchanged
-        fi
-    fi
+    nvm2 autoload
 }
 
 # 根据不同 shell 设置钩子
