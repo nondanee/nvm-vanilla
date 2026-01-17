c1ec89111cfbf867a1b4166fa86e6c665cffb1d4
nondanee <iminezn5656@outlook.com>
2026-01-18 00:12:07 +0800

    feat: nvm

--- src/init.sh
+++ src/init.sh
@@ -1,35 +1,35 @@
-nvm2() {
-    local cmd="$(command nvm2 --eval "$@")"
+nvm() {
+    local cmd="$(command nvm --eval "$@")"
     if [ -n "$cmd" ]; then
         eval $cmd
     else
-        command nvm2 "$@"
+        command nvm "$@"
     fi
 }
 
-nvm2_autoload_dir=""
+nvm_autoload_dir=""
 
-_nvm2_autoload_hook() {
+_nvm_autoload_hook() {
     local current_dir="$PWD"
     
-    if [ "$current_dir" = "$nvm2_autoload_dir" ]; then
+    if [ "$current_dir" = "$nvm_autoload_dir" ]; then
         return
     fi
 
-    nvm2 autoload
+    nvm autoload
 
-    nvm2_autoload_dir="$current_dir"
+    nvm_autoload_dir="$current_dir"
 }
 
 # 根据不同 shell 设置钩子
 case "$SHELL" in
     *bash*)
         # bash: 使用 PROMPT_COMMAND
-        PROMPT_COMMAND="_nvm2_autoload_hook; $PROMPT_COMMAND"
+        PROMPT_COMMAND="_nvm_autoload_hook; $PROMPT_COMMAND"
         ;;
     *zsh*)
         # zsh: 使用 chpwd 或 precmd 钩子
         autoload -U add-zsh-hook
-        add-zsh-hook chpwd _nvm2_autoload_hook
+        add-zsh-hook chpwd _nvm_autoload_hook
         ;;
 esac
