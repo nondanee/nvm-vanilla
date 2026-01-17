502aa60f2f80f45da981323358e8fd68a4cf17e3
nondanee <iminezn5656@outlook.com>
2026-01-18 00:14:06 +0800

    feat: init

---
+++ src/init.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+if (process.platform === 'win32') {
+
+} else {
+    // fs.appendFileSync('~/.bashrc', )
+}
+
+
+// ${ZDOTDIR:-$HOME}/.zshrc
+// $HOME/.profile
+// $HOME/.bashrc
