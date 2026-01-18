c9084309a55f1ddc39732c82ea4e5efa1818f7b3
nondanee <iminezn5656@outlook.com>
2026-01-18 20:01:32 +0800

    feat: 去掉注释

--- src/nvm.sh
+++ src/nvm.sh
@@ -21,14 +21,11 @@ _nvm_autoload_hook() {
     nvm_autoload_dir="$current_dir"
 }
 
-# 根据不同 shell 设置钩子
 case "$SHELL" in
     *bash*)
-        # bash: 使用 PROMPT_COMMAND
         PROMPT_COMMAND="_nvm_autoload_hook; $PROMPT_COMMAND"
         ;;
     *zsh*)
-        # zsh: 使用 chpwd 或 precmd 钩子
         autoload -U add-zsh-hook
         add-zsh-hook chpwd _nvm_autoload_hook
         ;;
