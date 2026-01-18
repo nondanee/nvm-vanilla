d45685465ea12bc4dab4a12f33236f97ed4f60c5
nondanee <iminezn5656@outlook.com>
2026-01-18 19:15:20 +0800

    feat: powershell autoload

--- src/nvm.ps1
+++ src/nvm.ps1
@@ -6,3 +6,32 @@ function nvm {
         nvm-vanilla @args
     }
 }
+
+$global:_nvmAutoloadDir = ""
+
+function _nvmAutoloadHook {
+    if (-not [Environment]::UserInteractive) { return }
+    
+    $currentDir = (Get-Location).Path
+    
+    if ($global:_nvmAutoloadDir -eq $currentDir) { return }
+
+    nvm autoload
+    
+    $global:_nvmAutoloadDir = $currentDir
+}
+
+$originalPrompt = Get-Item function:prompt -ErrorAction SilentlyContinue
+if ($originalPrompt) {
+    $originalPrompt = $originalPrompt.ScriptBlock
+}
+
+function global:prompt {
+    _nvmAutoloadHook
+    
+    if ($originalPrompt) {
+        & $originalPrompt
+    } else {
+        "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
+    }
+}
