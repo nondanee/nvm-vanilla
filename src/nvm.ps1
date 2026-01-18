7567dcda99ca667cdfb6204768b350df382084ec
nondanee <iminezn5656@outlook.com>
2026-01-18 16:57:43 +0800

    feat: join

--- src/nvm.ps1
+++ src/nvm.ps1
@@ -1,7 +1,7 @@
 function nvm {
     $result = nvm-vanilla --eval @args 2>$null
     if ($result) {
-        Invoke-Expression $result
+        Invoke-Expression ($result -join ";")
     } else {
         nvm-vanilla @args
     }
