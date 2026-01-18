a8f3d0a3057d8a0246cfdad0608184ba61c33d5f
nondanee <iminezn5656@outlook.com>
2026-01-18 17:00:59 +0800

    feat: line

--- src/init.js
+++ src/init.js
@@ -5,15 +5,13 @@ const { promisify } = require('util');
 
 const init = async () => {
     if (process.platform === 'win32') {
-        await promisify(fs.appendFile)(
-            path.resolve(os.homedir(), 'Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1'),
-            '\nnvm-vanilla --eval env | Out-String | Invoke-Expression',
-        )
+        const profilePath = path.resolve(os.homedir(), 'Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1');
+        let content = await promisify(fs.readFile)(profilePath, 'utf-8');
+        const line = 'nvm-vanilla --eval env | Out-String | Invoke-Expression';
     } else {
-        await promisify(fs.appendFile)(
-            path.resolve(os.homedir(), '.bashrc'),
-            '\neval "$(nvm-vanilla --eval env)"',
-        );
+        const profilePath = path.resolve(os.homedir(), '.bashrc');
+        let content = await promisify(fs.readFile)(profilePath, 'utf-8');
+        const line = 'eval "$(nvm-vanilla --eval env)"';
     }
 };
 
