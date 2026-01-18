ce3b277fd3e55c8f26400ea861da804ef977c888
nondanee <iminezn5656@outlook.com>
2026-01-18 17:11:29 +0800

    feat: 简化

--- src/init.js
+++ src/init.js
@@ -3,28 +3,25 @@ const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
 
-const insert = (content, line) => {
+const insert = async (profilePath, line) => {
+    let content = await promisify(fs.readFile)(profilePath, 'utf-8');
     if (/nvm-vanilla/.test(content)) {
         content = content.replace(/^.*nvm-vanilla.*$/m, line);
     } else {
         content += '\n' + line;
     }
-    return content;
+    await promisify(fs.writeFile)(profilePath, content, 'utf-8');
 };
 
 const init = async () => {
     if (process.platform === 'win32') {
         const profilePath = path.resolve(os.homedir(), 'Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1');
-        let content = await promisify(fs.readFile)(profilePath, 'utf-8');
         const line = 'nvm-vanilla --eval env | Out-String | Invoke-Expression';
-        content = insert(content, line);
-        await promisify(fs.writeFile)(profilePath, content, 'utf-8');
+        await insert(content, line);
     } else {
         const profilePath = path.resolve(os.homedir(), '.bashrc');
-        let content = await promisify(fs.readFile)(profilePath, 'utf-8');
         const line = 'eval "$(nvm-vanilla --eval env)"';
-        content = insert(content, line);
-        await promisify(fs.writeFile)(profilePath, content, 'utf-8');
+        await insert(content, line);
     }
 };
 
